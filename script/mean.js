c1ab73428da7d88a2d0b38120b43356ceac023f4
Aayam555 <63101665+Aayam555@users.noreply.github.com>
2021-08-09 14:48:59 +0545

    Updates

--- script/mean.js
+++ script/mean.js
@@ -1,10 +1,25 @@
 const calculateMean = () => {
   // Getting Value
-  let sumX = document.getElementById("sumX").value;
-  let number = document.getElementById("number").value;
+  let data = document.getElementById("data").value;
+
+  // Extracting the data from the user input
+  let dataArrayString = data.split(",");
+
+  let dataArrayInt = [];
+
+  for (let dataArrayIndex = 0; dataArrayIndex<dataArrayString.length; dataArrayIndex++){
+    dataArrayInt.push(parseInt(dataArrayString[dataArrayIndex].trim()));
+  }
+
+  // Calculating the sum of data
+  let sumX = 0
+
+  for (let dataIndex = 0; dataIndex < dataArrayInt.length; dataIndex++){
+    sumX += dataArrayInt[dataIndex];
+  }
 
   // Calculating Mean
-  let mean = sumX / number;
+  let mean = sumX/dataArrayString.length;
 
   // Displaying Mean
   document.getElementById("display").innerHTML = `Mean = ${mean}`;
