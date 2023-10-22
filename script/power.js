5b2e8793fcf88f2356d91bed48656d6f61380a85
Aayam Shrestha <63101665+Aayam555@users.noreply.github.com>
2023-10-22 16:30:05 +0545

    Delete script directory

--- script/power.js
+++
@@ -1,9 +0,0 @@
-function power() {
-    // Taking value from user
-    let base = document.getElementById('i1').value 
-    let exponent = document.getElementById('i2').value 
-    // Calculating
-    let power = base**exponent
-    // Displaying
-    document.getElementById('display').innerHTML = `${base}^${exponent} = ${power}`
-}
